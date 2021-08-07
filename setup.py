0cb2ccfe6a05b2da7e0e36fca66e546c2dcce9e7
Batuhan Olgaç <70446049+IMaresaLI@users.noreply.github.com>
2021-08-08 02:03:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="syscolors",
-    version="0.0.2",
+    version="0.0.5",
     author="Batuhan Olgac",
     author_email="mares4l@hotmail.com",
     description="Color your Python terminal screen as you wish.",
@@ -16,9 +16,17 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/IMaresaLI/syscolors/issues",
     },
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows"
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
